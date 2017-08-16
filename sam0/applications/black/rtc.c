6317468ecaf2dbc63abb6e89be9173e6f7682467
jin <dillonhua@gmail.com>
2017-08-16 18:00:35 +0800

    opt rtc code

--- sam0/applications/black/rtc.c
+++ sam0/applications/black/rtc.c
@@ -1,46 +1,25 @@
 #include <asf.h>
 struct rtc_module rtc_instance;
+struct rtc_calendar_alarm_time alarm;
 
-void count_next_one_hour(struct rtc_calendar_time *cur_time, 
-		struct rtc_calendar_time *next_time)
-{
-	next_time->hour 	= cur_time->hour + 1;
-	next_time->hour 	= next_time->hour % 24;
-	next_time->second 	= cur_time->second;
-	next_time->year 	= cur_time->year;
-	next_time->month 	= cur_time->month;
-	next_time->day 		= cur_time->day;
-	next_time->minute 	= cur_time->minute;
-}
 void rtc_match_callback(void)
 {
-	struct rtc_calendar_time cur_time;
-	struct rtc_calendar_alarm_time alarm;
-	rtc_calendar_get_time(&rtc_instance, &cur_time);
-	count_next_one_hour(&cur_time, &next_time);
-
-	alarm.time.year      = next_time.year;
-	alarm.time.month     = next_time.month;
-	alarm.time.day       = next_time.day;
-	alarm.time.hour      = next_time.hour;
-	alarm.time.minute    = next_time.minute;
-	alarm.time.second    = next_time.second;
+	alarm.time.hour 	+= 1;
+	alarm.time.hour 	= alarm.time.hour % 24;
 	alarm.mask = RTC_CALENDAR_ALARM_MASK_HOUR;	
 	rtc_calendar_set_alarm(&rtc_instance, &alarm, RTC_CALENDAR_ALARM_0);
 }
 bool set_rtc_time(struct rtc_calendar_time cur_time)
 {
 	static bool init = false;
-	struct rtc_calendar_time next_time;
-	struct rtc_calendar_alarm_time alarm;
 	
-	count_next_one_hour(&cur_time, &next_time);
-	alarm.time.year      = next_time.year;
-	alarm.time.month     = next_time.month;
-	alarm.time.day       = next_time.day;
-	alarm.time.hour      = next_time.hour;
-	alarm.time.minute    = next_time.minute;
-	alarm.time.second    = next_time.second;
+	alarm.time.year      = cur_time.year;
+	alarm.time.month     = cur_time.month;
+	alarm.time.day       = cur_time.day;
+	alarm.time.hour      = cur_time.hour + 1;
+	alarm.time.hour 	 = alarm.time.hour % 24;
+	alarm.time.minute    = cur_time.minute;
+	alarm.time.second    = cur_time.second;
 	alarm.mask 			 = RTC_CALENDAR_ALARM_MASK_HOUR;
 
 	if (!init)
