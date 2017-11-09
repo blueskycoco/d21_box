6f0ee4254b9fe5525a43d2685483f6bddafcb8c1
jin <dillonhua@gmail.com>
2017-11-09 08:33:37 +0800

    update toHex

--- sam0/applications/black/misc.c
+++ sam0/applications/black/misc.c
@@ -40,7 +40,7 @@ void console_init(void)
 }
 #endif
 char *build_json(char *old, char type, uint32_t bloodSugar, int actionTime, int gid, 
-				char *device_id)
+		char *device_id)
 {
 #if 1
 	sprintf(old, JSON,type,bloodSugar,actionTime,gid,device_id);
@@ -111,7 +111,7 @@ bool do_it(uint8_t *in, uint32_t *time)
 			cJSON_Delete(root);			
 	}
 	return result;
-	#endif
+#endif
 }
 void ts2date(uint32_t time, struct rtc_calendar_time *date_out)
 {
@@ -134,24 +134,39 @@ void ts2date(uint32_t time, struct rtc_calendar_time *date_out)
 	date_out->year= atoi(tmp);
 }
 /*uint32_t date2ts(struct rtc_calendar_time date)
-{
-	struct calendar_date cur_date;
-	cur_date.date = date.day;
-	cur_date.hour = date.hour;
-	cur_date.minute = date.minute;
-	cur_date.month = date.month;
-	cur_date.second = date.second;
-	cur_date.year = date.year;
-	return calendar_date_to_timestamp(&cur_date);
-}*/
+  {
+  struct calendar_date cur_date;
+  cur_date.date = date.day;
+  cur_date.hour = date.hour;
+  cur_date.minute = date.minute;
+  cur_date.month = date.month;
+  cur_date.second = date.second;
+  cur_date.year = date.year;
+  return calendar_date_to_timestamp(&cur_date);
+  }*/
 void toHex(uint8_t *input, uint32_t len, uint8_t *output)
 {
-	int i = 0, j = 0;
-	printf("toHex\r\n");
-	for (i=0; i<len; i++) {
-		printf(">%02x\r\n",input[i]);
-		output[j++] = ((input[i] >> 4) & 0x0f) + 0x30;
-		output[j++] = (input[i] & 0x0f) + 0x30;
-		printf("<%02x%02x\r\n",output[j-2],output[j-1]);
+	int ix=0,iy=0;
+	for (ix=0; ix<len; ix++) {
+		output[iy] = (input[ix]&0xf0) >>4;
+		if(output[iy]>=10 && output[iy]<=15)
+		{
+			output[iy] = output[iy] + 'a' - 10;
 		}
+		else
+		{
+			output[iy] = output[iy] + '0';
+		}
+		iy++;
+		output[iy] = input[ix]&0x0f;
+		if(output[iy] >= 10 && output[iy] <= 15)
+		{
+			output[iy] = output[iy] + 'a' - 10;
+		}
+		else
+		{
+			output[iy] = output[iy] + '0';
+		}
+		iy++;
+	}
 }
